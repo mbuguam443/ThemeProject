26fb070c3bf5bb013c1cef88581fc58579d50210
Mathew Mbugua <mbuguam443@gmail.com>
2025-07-26 19:53:34 +0300

    setting update1

--- Themeproject/settings.py
+++ Themeproject/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = 'static/'
 
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
