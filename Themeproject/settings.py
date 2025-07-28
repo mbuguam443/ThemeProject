7c2612478b8a64250bd5731ec1aeba9da2f8e2e8
Mathew Mbugua <mbuguam443@gmail.com>
2025-07-28 18:20:13 +0300

    setting update

--- Themeproject/settings.py
+++ Themeproject/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # For production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
